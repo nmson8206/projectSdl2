c3b371c7de6f07dd03c6980de510f8ce3be4eff2
nmson8206 <nmson8206@gmail.com>
2025-04-23 23:09:26 +0700

    commit

--- prj/main.cpp
+++ prj/main.cpp
@@ -12,7 +12,7 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int FRAME_WIDTH = 80;
 const int FRAME_HEIGHT = 80;
-const int GROUND_Y = SCREEN_HEIGHT - 100;
+const int GROUND_Y = SCREEN_HEIGHT - 1;
 
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
@@ -21,7 +21,8 @@ SDL_Texture* background = nullptr;
 enum PlayerState {
     PLAYER_IDLE,
     PLAYER_RUNNING,
-    PLAYER_JUMPING
+    PLAYER_JUMPING,
+    PLAYER_ATTACKING
 };
 
 struct Player {
@@ -29,13 +30,22 @@ struct Player {
     int vx = 0, vy = 0;
     int frame = 0;
     int frameCounter = 0;
+    int attackCombo = 0;
+    int attackTimer = 0;
     bool facingRight = true;
     bool onGround = true;
+    bool attacking = false;
+    int comboStep = 0;
+    Uint32 lastAttackTime = 0;
+    bool isAttacking = false;
     PlayerState state = PLAYER_IDLE;
 
     vector<SDL_Texture*> idleFrames;
     vector<SDL_Texture*> runFrames;
     vector<SDL_Texture*> jumpFrames;
+    vector<SDL_Texture*> attack1Frames;
+    vector<SDL_Texture*> attack2Frames;
+    vector<SDL_Texture*> attack3Frames;
 };
 
 enum EnemyState {
@@ -77,6 +87,9 @@ void loadPlayerFrames(Player& player) {
     loadFrames(player.idleFrames, "idle", 6);
     loadFrames(player.runFrames, "run", 8);
     loadFrames(player.jumpFrames, "jump", 9);
+    loadFrames(player.attack1Frames, "attack(1)", 4);
+    loadFrames(player.attack2Frames, "attack(2)", 5);
+    loadFrames(player.attack3Frames, "attack(3)", 4);
 }
 
 void loadEnemyFrames(Enemy& enemy) {
@@ -85,17 +98,21 @@ void loadEnemyFrames(Enemy& enemy) {
 }
 
 void handlePlayerInput(Player& player, const Uint8* keystate) {
+    if (player.isAttacking) {
+        player.vx = 0;
+        return;
+    }
     player.vx = 0;
 
     if (keystate[SDL_SCANCODE_A]) {
         player.vx = -4;
         player.facingRight = false;
-        if (player.onGround) player.state = PLAYER_RUNNING;
+        if (player.onGround && !player.isAttacking) player.state = PLAYER_RUNNING;
     } else if (keystate[SDL_SCANCODE_D]) {
         player.vx = 4;
         player.facingRight = true;
-        if (player.onGround) player.state = PLAYER_RUNNING;
-    } else if (player.onGround) {
+        if (player.onGround && !player.isAttacking) player.state = PLAYER_RUNNING;
+    } else if (player.onGround && !player.isAttacking) {
         player.state = PLAYER_IDLE;
     }
 
@@ -106,49 +123,102 @@ void handlePlayerInput(Player& player, const Uint8* keystate) {
         player.frame = 0;
         player.frameCounter = 0;
     }
+
+    if (keystate[SDL_SCANCODE_J] && !player.isAttacking) {
+        player.isAttacking = true;
+        player.comboStep = (player.comboStep % 3) + 1;
+        player.frame = 0;
+        player.frameCounter = 0;
+        player.state = PLAYER_ATTACKING;
+        player.lastAttackTime = SDL_GetTicks();
+    }
 }
+
 
 void updatePlayer(Player& player) {
     player.x += player.vx;
     player.vy += 1;
     player.y += player.vy;
-
+    player.frameCounter++;
+    int delay = 6;
     if (player.y >= GROUND_Y - FRAME_HEIGHT) {
         player.y = GROUND_Y - FRAME_HEIGHT;
         player.vy = 0;
         player.onGround = true;
-        if (player.vx == 0) player.state = PLAYER_IDLE;
-        else player.state = PLAYER_RUNNING;
+        if (player.vx == 0 && !player.isAttacking) player.state = PLAYER_IDLE;
+        else if (!player.isAttacking) player.state = PLAYER_RUNNING;
     }
 
-    player.frameCounter++;
-    int delay = 8;
+    if (player.isAttacking && SDL_GetTicks() - player.lastAttackTime > 600) {
+        player.comboStep = 0;
+        player.isAttacking = false;
+        player.state = PLAYER_IDLE;
+        player.frame = 0;
+    }
+    if (player.state == PLAYER_ATTACKING){
+        if (player.comboStep == 2){
+            delay = 4;
+        }
+    }
     if (player.frameCounter >= delay) {
         player.frameCounter = 0;
-        if (player.state == PLAYER_IDLE && !player.idleFrames.empty())
-            player.frame = (player.frame + 1) % player.idleFrames.size();
-        else if (player.state == PLAYER_RUNNING && !player.runFrames.empty())
-            player.frame = (player.frame + 1) % player.runFrames.size();
-        else if (player.state == PLAYER_JUMPING && !player.jumpFrames.empty())
-            player.frame = (player.frame + 1) % player.jumpFrames.size();
+
+        if (player.state == PLAYER_ATTACKING) {
+            int frameLimit = 0;
+            if (player.comboStep == 1) frameLimit = player.attack1Frames.size();
+            else if (player.comboStep == 2) frameLimit = player.attack2Frames.size();
+            else if (player.comboStep == 3) frameLimit = player.attack3Frames.size();
+
+            player.frame++;
+            if (player.frame >= frameLimit) {
+                player.isAttacking = false;
+                player.frame = 0;
+                player.state = PLAYER_IDLE;
+                player.lastAttackTime = SDL_GetTicks();
+            }
+        } else {
+            if (player.state == PLAYER_IDLE && !player.idleFrames.empty())
+                player.frame = (player.frame + 1) % player.idleFrames.size();
+            else if (player.state == PLAYER_RUNNING && !player.runFrames.empty())
+                player.frame = (player.frame + 1) % player.runFrames.size();
+            else if (player.state == PLAYER_JUMPING && !player.jumpFrames.empty())
+                player.frame = (player.frame + 1) % player.jumpFrames.size();
+        }
     }
 }
 
-void renderPlayer(const Player& player) {
-    SDL_Rect dest = { player.x, player.y, FRAME_WIDTH, FRAME_HEIGHT };
-    SDL_RendererFlip flip = player.facingRight ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL;
 
+
+void renderPlayer(const Player& player) {
     SDL_Texture* currentFrame = nullptr;
+    SDL_RendererFlip flip = player.facingRight ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL;
+    SDL_Rect dest = { player.x, player.y, FRAME_WIDTH, FRAME_HEIGHT };
+
     if (player.state == PLAYER_IDLE && !player.idleFrames.empty())
         currentFrame = player.idleFrames[player.frame % player.idleFrames.size()];
     else if (player.state == PLAYER_RUNNING && !player.runFrames.empty())
         currentFrame = player.runFrames[player.frame % player.runFrames.size()];
     else if (player.state == PLAYER_JUMPING && !player.jumpFrames.empty())
         currentFrame = player.jumpFrames[player.frame % player.jumpFrames.size()];
+    else if (player.state == PLAYER_ATTACKING) {
+        if (player.comboStep == 1 && !player.attack1Frames.empty()) {
+            currentFrame = player.attack1Frames[player.frame % player.attack1Frames.size()];
+            dest.w = 112; dest.h = 80;
+        } else if (player.comboStep == 2 && !player.attack2Frames.empty()) {
+            currentFrame = player.attack2Frames[player.frame % player.attack2Frames.size()];
+            dest.w = 112;
+            dest.h = 112;
+            dest.y = player.y - (112 - FRAME_HEIGHT);
+        } else if (player.comboStep == 3 && !player.attack3Frames.empty()) {
+            currentFrame = player.attack3Frames[player.frame % player.attack3Frames.size()];
+            dest.w = 112; dest.h = 80;
+        }
+    }
 
     if (currentFrame)
         SDL_RenderCopyEx(renderer, currentFrame, nullptr, &dest, 0.0, nullptr, flip);
 }
+
 
 void updateEnemy(Enemy& enemy, const Player& player) {
     if (!enemy.onGround) {
@@ -207,7 +277,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
     IMG_Init(IMG_INIT_PNG);
 
-    window = SDL_CreateWindow("Enemy Falling Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    window = SDL_CreateWindow("Samurai Vengeance", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     background = loadTexture("background.png");
@@ -252,6 +322,9 @@ int main(int argc, char* argv[]) {
     for (auto tex : player.idleFrames) SDL_DestroyTexture(tex);
     for (auto tex : player.runFrames) SDL_DestroyTexture(tex);
     for (auto tex : player.jumpFrames) SDL_DestroyTexture(tex);
+    for (auto tex : player.attack1Frames) SDL_DestroyTexture(tex);
+    for (auto tex : player.attack2Frames) SDL_DestroyTexture(tex);
+    for (auto tex : player.attack3Frames) SDL_DestroyTexture(tex);
     for (auto tex : enemy.runFrames) SDL_DestroyTexture(tex);
     for (auto tex : enemy.fallingFrames) SDL_DestroyTexture(tex);
 
